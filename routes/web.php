02e04b3c35429be13475e1ebe9b13cbbcd9e0941
chathura-ce <chathu.eac@gmail.com>
2020-11-06 15:06:25 +0530

    Reading data

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// insert data
 Route::get('/create', function () {
     $user = User::findOrFail(1);
 //    $post = new Post(['title'=>'php','body'=>'this is a body']);
 //    $user->posts()->save($post);
 
 //    or
-    
+
     $user->posts()->save( new Post(['title'=>'php','body'=>'this is a body 2']));
+});
+
+//Reading data
+Route::get('/read',function (){
+    $user = User::findOrfail(1);
+
+//    return $user->posts;
+//    dd($user);
+    foreach ($user->posts as $post){
+        echo $post->title . "<br>";
+    }
 });
